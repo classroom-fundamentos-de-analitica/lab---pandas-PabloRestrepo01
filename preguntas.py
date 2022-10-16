9f3df97decdf81090f1914c4a89c514a14743181
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2022-10-16 15:15:16 -0500

    10!

--- preguntas.py
+++ preguntas.py
@@ -151,7 +151,7 @@ def pregunta_09():
     tbl0['year'] = tbl0['_c3'].map(lambda x: int(x.split('-')[0]))
     return tbl0
 
-
+print(pregunta_09())
 def pregunta_10():
     """
     Construya una tabla que contenga _c1 y una lista separada por ':' de los valores de
